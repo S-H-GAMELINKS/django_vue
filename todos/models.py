3d9681ff7fe892414fbf310abe36d79eaacfa682
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-02 17:45:09 +0900

    fix Todo model

--- todos/models.py
+++ todos/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
-class Todo(mdoels.Model):
+class Todo(models.Model):
     title_text = models.CharField(max_length=30)
     content_text = models.CharField(max_length=140)
