5646ac902e4d5c35f4e9da668bebf9be04cb8249
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-04 13:22:45 +0900

    using vue components

--- static/webpack.config.js
+++ static/webpack.config.js
@@ -1,3 +1,5 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
 module.exports = {
     entry: './src/index.js',
     output: {
@@ -6,6 +8,10 @@ module.exports = {
     },
     module: {
         rules: [
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
             {
                 test: /\.css$/,
                 use: [
