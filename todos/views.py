2ddcbcfef8f7abec78765a4b0f4c698d157f1974
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-05 17:12:25 +0900

    add todo restful view

--- todos/views.py
+++ todos/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Todo
+from rest_framework import viewsets
+from .serializers import TodoSerializer
 
 def index(request):
     return render(request, 'index.html')
@@ -8,3 +11,7 @@ def about(request):
 
 def contact(request):
     return render(request, 'index.html')
+
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
