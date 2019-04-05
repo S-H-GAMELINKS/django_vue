9ff13691a3f6c0e7f767eff815afc6ac14dae551
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-05 17:24:25 +0900

    set restful api

--- todos/urls.py
+++ todos/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path 
-
+from django.urls import path, include 
+from rest_framework import routers
 from . import views
+
+router = routers.DefaultRouter()
+router.register(r'todos', views.TodoViewSet)
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
     path('contact', views.contact, name='contact'),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
